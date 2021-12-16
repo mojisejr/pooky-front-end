7c8aea74e6b7bd7cab6684e431fa757e4d518ea5
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2021-12-16 23:35:25 +0700

    change image extension

--- components/layout/Home.js
+++ components/layout/Home.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import pookyLogo from "../../images/Logo.png";
-import planet1 from "../../images/MainPicture1.png";
+import planet1 from "../../images/MainPicture1.PNG";
 import stars from "../../images/bgStar.PNG";
 import Menu from "../Menu";
 
